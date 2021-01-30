9aba33f140963d0bc82227df51721714452b5b28
ShankarBinnar <shankar11.tech@gmail.com>
2021-01-30 13:12:42 +0530

    read_write_operations by shankar

---
+++ file/read_file.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<error.h>
+
+int main () {
+   FILE *fp;
+   char string[20];
+   fp = fopen("file.txt", "r+");
+   if(fp==NULL)
+	{
+		perror("file not opened.\n");
+		return (-1);
+	}
+   	printf("shankar\n");
+   fputs("This is c programming.", fp);
+   fputs("This is a system programming language.", fp);
+
+   fclose(fp);
+   fp=fopen("file.txt","r+");
+
+	if(fp==NULL)
+	{
+		perror("file not opened.\n");
+		return (-1);
+	}
+	while(fgets(string,20,fp)!=NULL)
+	{
+		puts(string);
+	}
+	fclose(fp);
+	printf("exiting");
+	return 0;
+}	
